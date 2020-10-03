4e2788f9e9c58978e2247773d858c58c03951468
Leigh Halliday <leighhalliday@gmail.com>
2020-10-03 15:13:57 -0400

    NextAuth Demo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { Provider } from "next-auth/client";
+import "../styles/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import Adapters from "next-auth/adapters";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default (req, res) =>
+  NextAuth(req, res, {
+    providers: [
+      Providers.GitHub({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      }),
+    ],
+    debug: process.env.NODE_ENV === "development",
+    secret: process.env.AUTH_SECRET,
+    jwt: {
+      secret: process.env.JWT_SECRET,
+    },
+    adapter: Adapters.Prisma.Adapter({ prisma }),
+  });
